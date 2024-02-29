a7855c792aa486f1b87f72d744fae5a76595f73d
Nitinrathore27 <aahara.nitin.27@gmail.com>
2024-02-29 11:10:33 +0530

    Second commint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React, { useState, useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login , logout} from './store/authSlice'
+import { Outlet } from 'react-router-dom'
+import {Header, Footer} from './components/index'
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const [loading, setloading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentuser()
+    .then((userData)=>{
+      if(userData){
+        // console.log("hii")
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setloading(false))
+  }, [])
+  
+  return !loading ? 
+  <div className='min-h-screen bg-gray-600 content-between flex flex-wrap'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        Todo <Outlet/>
+      </main>
+      <Footer/>
+    </div>
+  </div> : null
 }
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Container from "./Container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+export  {
+    Footer, 
+    Header,
+    Container,
+    LogoutBtn,
+    Logo,
+}
